b92b3daf2155db42bdb81dc2b791adf83a389a27
nemanja2424 <njakovlje@gmail.com>
2026-04-26 20:23:55 +0200

    logo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -29,6 +29,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="sr" data-scroll-behavior="smooth" className={`${geistSans.variable} ${geistMono.variable}`}>
       <head>
+        <link rel="icon" href="/brateraLogo.webp" type="image/webp" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
       </head>
       <body>
